9e9945c3778384a92140577f1e0f1aecc43e4b6d
Marco Islas <marco3islas@gmail.com>
2025-05-14 12:33:16 -0600

    Configurar PostgreSql para Railway

--- blog/settings.py
+++ blog/settings.py
@@ -62,7 +62,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhitenoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
