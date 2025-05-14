1629d6539b8d95c24d9564f99b82f4547c801100
Marco Islas <marco3islas@gmail.com>
2025-05-14 12:31:09 -0600

    Configurar PostgreSql para Railway

--- blog/settings.py
+++ blog/settings.py
@@ -152,10 +152,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "blog_application/static/",
-]
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
